84465c59aaaad2b2d39ebd2134e2fa167c17ed00
Jacob Smith <jacob@jacobsmith.io>
2015-07-11 16:32:17 -0400

    Implement basic test for creating object

--- tests/integration/creating-objects-test.js
+++ tests/integration/creating-objects-test.js
@@ -1,23 +1,36 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
 import startApp from 'dummy/tests/helpers/start-app';
-import mockParse from 'dummy/tests/helpers/mock-parse';
-import unmockParse from 'dummy/tests/helpers/unmock-parse';
+import MockParse from '../helpers/mock-parse';
 
-var application;
+var application, container;
 
 module('Integration | creating objects', {
   beforeEach: function() {
-    mockParse();
     application = startApp();
+    MockParse.setup(application);
+    // TODO: figure out the correct way to access the store (unless this is the correct way)
+    this.store = application.__container__.lookup('store:main');
   },
 
   afterEach: function() {
-    unmockParse();
+    MockParse.teardown(application);
     Ember.run(application, 'destroy');
   }
 });
 
 test('creating an object without relationships', function(assert) {
-  assert.ok(true);
+  var done = assert.async();
+
+  Ember.run(()=> {
+    var subject = this.store.createRecord('simple', {name: "Simple"});
+    subject.save().then(()=> {
+
+      assert.equal("2012-12-12T12:12:12.000Z", subject.get('createdAt').toISOString());
+      assert.equal("Simple", subject.get('name'));
+      assert.equal(1, subject.id);
+
+      done();
+    });
+  });
 });
